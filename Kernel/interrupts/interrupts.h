866bf3a0f11d223c3044bcf7720f10310361c080
FiReLScar <levicowan2005@icloud.com>
2021-04-29 22:47:07 -0500

    Revert "Merge branch 'main' of https://github.com/Null-LLC/Hydra into main"
    This reverts commit a4242ac3ee6c2ac086fc46d9900b12183d66a78b, reversing
    changes made to 65c30340ff54b95a0e2a3c917a3bc9f74c3885de.

--- Kernel/interrupts/interrupts.h
+++ Kernel/interrupts/interrupts.h
@@ -12,11 +12,10 @@
 #define ICW4_8086 0x01
 
 struct interrupt_frame;
-__attribute__((interrupt)) void PageFault_Handler(interrupt_frame* frame);
-__attribute__((interrupt)) void DoubleFault_Handler(interrupt_frame* frame);
-__attribute__((interrupt)) void GPFault_Handler(interrupt_frame* frame);
-__attribute__((interrupt)) void KeyboardInt_Handler(interrupt_frame* frame);
-__attribute__((interrupt)) void MouseINt_Handler(interrupt_frame* frame);
+__attribute__((interrupt)) void PageFault_Handler(struct interrupt_frame* frame);
+__attribute__((interrupt)) void DoubleFault_Handler(struct interrupt_frame* frame);
+__attribute__((interrupt)) void GPFault_Handler(struct interrupt_frame* frame);
+__attribute__((interrupt)) void KeyboardInt_Handler(struct interrupt_frame* frame);
 
 void RemapPIC();
 void PIC_EndMaster();
