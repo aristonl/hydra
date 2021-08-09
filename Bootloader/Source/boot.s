04108a3264443a1169278395eaec212e98080bbc
Levi Hicks <levih@n11.dev>
2021-08-09 09:19:28 -0500

    Wow, took forever!
    I replaced includes with actual code, and made things a bit cleaner, I removed the debug sheet and all of the codes.
    And finally added 64 bit support!

--- Bootloader/Source/boot.s
+++ Bootloader/Source/boot.s
@@ -1,20 +1,20 @@
 [org 0x7c00]
-
 mov [BootDisk], dl
-
-mov ah, 0x0e
-mov al, '0'
-int 0x10
-
 mov bp, 0x7c00
 mov sp, bp
-
-call ReadDisk
-
+mov ah, 0x02
+mov bx, 0x7e00
+mov al, 4
+mov dl, [BootDisk]
+mov ch, 0x00
+mov dh, 0x00
+mov cl, 0x02
+int 0x13
+jc error
 jmp 0x7e00
-
-%include "disk.s"
-
+BootDisk:
+  db 0
+error:
+  jmp $
 times 510-($-$$) db 0
-
 dw 0xaa55
--- Bootloader/Source/extension.s
+++ Bootloader/Source/extension.s
@@ -1,13 +1,35 @@
 [org 0x7e00]
-
-mov ah, 0x0e
-mov al, '2'
-int 0x10
-
 jmp EnterProtectedMode
-
-%include "gdt.s"
-
+GDTNull:
+  dd 0
+  dd 0
+GDTCode:
+  dw 0xFFFF
+  dw 0x0000
+  db 0x00
+  db 10011010b
+  db 11001111b
+  db 0x00
+GDTData:
+  dw 0xFFFF
+  dw 0x0000
+  db 0x00
+  db 10010010b
+  db 11001111b
+  db 0x00
+GDTEnd:
+GDT:
+  GDTSize:
+    dw GDTEnd - GDTNull - 1
+    dq GDTNull
+CodeSegment equ GDTCode - GDTNull
+DataSegment equ GDTData - GDTNull
+[bits 32]
+Set64BitGDT:
+  mov [GDTCode + 6], byte 10101111b
+  mov [GDTCode + 6], byte 10101111b
+  ret
+[bits 16]
 EnterProtectedMode:
   call EnableA20
   cli
@@ -16,13 +38,11 @@ EnterProtectedMode:
   or eax, 1
   mov cr0, eax
   jmp CodeSegment:StartProtectedMode
-
 EnableA20:
   in al, 0x92
   or al, 2
   out 0x92, al
   ret
-
 [bits 32]
 StartProtectedMode:
   mov ax, DataSegment
@@ -31,7 +51,95 @@ StartProtectedMode:
   mov es, ax
   mov fs, ax
   mov gs, ax
-  mov [0xb8000], byte '3'
+  pushfd
+  pop eax
+  mov ecx, eax
+  xor eax, 1 << 21
+  push eax
+  popfd
+  pushfd
+  pop eax
+  push ecx
+  popfd
+  xor eax, ecx
+  jz NoCPUID
+  mov eax, 0x80000001
+  cpuid
+  test edx, 1 << 29
+  jz NoLongMode
+  mov edi, 0x1000
+  mov cr3, edi
+  mov dword[edi], 0x2003
+  add edi, 0x1000
+  mov dword[edi], 0x3003
+  add edi, 0x1000
+  mov dword[edi], 0x4003
+  add edi, 0x1000
+  mov ebx, 0x00000003
+  mov ecx, 512
+  .SetEntry:
+    mov dword[edi], ebx
+    add ebx, 0x1000
+    add edi, 8
+    loop .SetEntry
+  mov eax, cr4
+  or eax, 1 << 5
+  mov cr4, eax
+  mov ecx, 0xC0000080
+  rdmsr
+  or eax, 1 << 8
+  wrmsr
+  mov eax, cr0
+  or eax, 1 << 31
+  mov cr0, eax
+  call Set64BitGDT
+  jmp CodeSegment:Start64Bit
+NoLongMode:
+  mov [0xb8000], byte 'N'
+  mov [0xb8001], byte 0x1F
+  mov [0xb8002], byte 'o'
+  mov [0xb8003], byte 0x1F
+  mov [0xb8004], byte ' '
+  mov [0xb8005], byte 0x1F
+  mov [0xb8006], byte 'L'
+  mov [0xb8007], byte 0x1F
+  mov [0xb8008], byte 'o'
+  mov [0xb8009], byte 0x1F
+  mov [0xb800A], byte 'n'
+  mov [0xb800B], byte 0x1F
+  mov [0xb800C], byte 'g'
+  mov [0xb800D], byte 0x1F
+  mov [0xb800E], byte ' '
+  mov [0xb800F], byte 0x1F
+  mov [0xb8010], byte 'M'
+  mov [0xb8011], byte 0x1F
+  mov [0xb8012], byte 'o'
+  mov [0xb8013], byte 0x1F
+  mov [0xb8014], byte 'd'
+  mov [0xb8015], byte 0x1F
+  mov [0xb8016], byte 'e'
+  mov [0xb8017], byte 0x1F
+  hlt
+NoCPUID:
+  mov [0xb8000], byte 'N'
+  mov [0xb8001], byte 0x1F
+  mov [0xb8002], byte 'o'
+  mov [0xb8003], byte 0x1F
+  mov [0xb8004], byte ' '
+  mov [0xb8005], byte 0x1F
+  mov [0xb8006], byte 'C'
+  mov [0xb8007], byte 0x1F
+  mov [0xb8008], byte 'P'
+  mov [0xb8009], byte 0x1F
+  mov [0xb800A], byte 'U'
+  mov [0xb800B], byte 0x1F
+  mov [0xb800C], byte 'I'
+  mov [0xb800D], byte 0x1F
+  mov [0xb800E], byte 'D'
+  mov [0xb800F], byte 0x1F
+  hlt
+[bits 64]
+Start64Bit:
+  
   jmp $
-
 times 2048-($-$$) db 0
