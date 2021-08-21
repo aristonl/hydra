b454d01299dbc357e491513ec646125b155bb187
Ariston Lorenzo <me@ariston.dev>
2021-08-21 01:30:37 -0400

    LibC: starting work on libc
    Signed-off-by: Thomas Lim <lxsthomaslim@gmail.com>

---
+++ Libraries/LibC/sys/cdefs.h
@@ -0,0 +1,24 @@
+/* Copyright (c) 2021 Null. All rights reserved. 
+ * Copyright (c) 2021 Echo/Inferno/B.O.B. Developers.
+ */
+/* 
+ * Copyright (c) 2020 Optix. 
+ * Licensed under the Optix Public License.
+ */
+/* 
+ * cdefs.h
+ *
+ * Created by Ariston Lorenzo on 1/9/2020.
+ * Copyright 2020 Ariston Lorenzo.
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+#define __BEGIN_DECLS extern "C" {
+    #define __END_DECLS
+}
+#else
+#define __BEGIN_DECLS
+#define __END_DECLS
+#endif
