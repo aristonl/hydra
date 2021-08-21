c3c7da393f17775130b7eacab2c6cdda3514b76b
Ariston Lorenzo <me@ariston.dev>
2021-08-21 18:00:07 -0400

    LibC: errno implementation :^)
    Signed-off-by: Thomas Lim <lxsthomaslim@gmail.com>

---
+++ Libraries/LibC/errno.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <sys/cdefs.h>
+#include <Kernel/errno.h>
+
+#define __RETURN_WITH_ERRNO(rc, good_ret, bad_ret) \
+    do { \
+        if (rc < 0) { \
+            errno = -rc; \
+            return (bad_ret); \
+        } else { \
+            errno = 0; \
+            return (good_ret); \
+        } \
+    } while(0)
+
+__BEGIN_DECLS
+
+extern int errno;
+
+__END_DECLS
