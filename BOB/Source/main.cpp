b1acb82fece81845a035bde2284d7cdedc3090fe
Levi Hicks <levih@n11.dev>
2021-09-01 21:21:33 -0500

    Clean the bootloader
    It was almost like it was yelling at me with all the completely capped variables and types "EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL"

--- BOB/Source/main.cpp
+++ BOB/Source/main.cpp
@@ -1,57 +1,40 @@
 #define UNICODE
+typedef struct Input {} Input;
+struct Output;
+typedef unsigned long long(*ResetOutput) (struct Output *This, unsigned char ExtendedVerification);
+typedef unsigned long long(*PrintOutput) (struct Output *This, unsigned short int *String);
+typedef unsigned long long(*TestOutput) (struct Output *This, unsigned short int *String);
+typedef unsigned long long(*QueryOutput) (struct Output *This, unsigned long long ModeNumber, unsigned long long *Columns, unsigned long long *Rows);
+typedef unsigned long long(*SetOutput) (struct Output *This, unsigned long long ModeNumber);
+typedef unsigned long long(*Font) (struct Output *This, unsigned long long Attribute);
+typedef struct Output {
+  ResetOutput Reset;
+  PrintOutput Print;
+  TestOutput TestString;
+  QueryOutput QueryMode;
+  SetOutput SetMode;
+  Font SetFont;
+} Output;
+typedef struct TableHeaders {
+  unsigned long long Signature;
+  unsigned int Revision;
+  unsigned int HeaderSize;
+  unsigned int CRC32;
+  unsigned int Reserved;
+} TableHeaders;
+typedef struct SystemTable {
+  TableHeaders hrd;
+  unsigned short int *FirmwareVendor;
+  unsigned int FirmwareVersion;
+  void* ConsoleInHandle;
+  Input *ConIn;
+  void* ConsoleOutHandle;
+  Output *ConOut;
+} SystemTable;
 
-typedef unsigned short int uint16_t;
-typedef unsigned short int uint_least16_t;
-typedef uint_least16_t CHAR16;
-typedef unsigned int UINT32;
-typedef unsigned long long UINT64;
-typedef unsigned long long UINTN;
-typedef unsigned char BOOLEAN;
-typedef void *EFI_HANDLE;
-typedef UINT64 EFI_STATUS;
-
-typedef struct EFI_SIMPLE_TEXT_INPUT_PROTOCOL {} EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
-
-struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
-
-typedef EFI_STATUS(*EFI_TEXT_RESET) (struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, BOOLEAN ExtendedVerification);
-typedef EFI_STATUS(*EFI_TEXT_STRING) (struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, CHAR16 *String);
-typedef EFI_STATUS(*EFI_TEXT_TEST_STRING) (struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, CHAR16 *String);
-typedef EFI_STATUS(*EFI_TEXT_QUERY_MODE) (struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, UINTN ModeNumber, UINTN *Columns, UINTN *Rows);
-typedef EFI_STATUS(*EFI_TEXT_SET_MODE) (struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, UINTN ModeNumber);
-typedef EFI_STATUS(*EFI_TEXT_SET_ATTRIBUTE) (struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, UINTN Attribute);
-
-typedef struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
-  EFI_TEXT_RESET Reset;
-  EFI_TEXT_STRING OutputString;
-  EFI_TEXT_TEST_STRING TestString;
-  EFI_TEXT_QUERY_MODE QueryMode;
-  EFI_TEXT_SET_MODE SetMode;
-  EFI_TEXT_SET_ATTRIBUTE SetAttribute;
-} EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
-
-typedef struct EFI_TABLE_HEADER {
-  UINT64 Signature;
-  UINT32 Revision;
-  UINT32 HeaderSize;
-  UINT32 CRC32;
-  UINT32 Reserved;
-} EFI_TABLE_HEADER;
-
-typedef struct EFI_SYSTEM_TABLE {
-  EFI_TABLE_HEADER hrd;
-  CHAR16 *FirmwareVendor;
-  UINT32 FirmwareVersion;
-  EFI_HANDLE ConsoleInHandle;
-  EFI_SIMPLE_TEXT_INPUT_PROTOCOL *ConIn;
-  EFI_HANDLE ConsoleOutHandle;
-  EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut;
-} EFI_SYSTEM_TABLE;
-
-extern "C" EFI_STATUS boot(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-  SystemTable->ConOut->Reset(SystemTable->ConOut, 1);
-  SystemTable->ConOut->SetAttribute(SystemTable->ConOut, 0x7e);
-  SystemTable->ConOut->OutputString(SystemTable->ConOut, (CHAR16*) L"Hello, world!\r\n");
+extern "C" unsigned long long boot(void* ImageHandle, SystemTable* systemTable) {
+  systemTable->ConOut->Reset(systemTable->ConOut, 1);
+  systemTable->ConOut->Print(systemTable->ConOut, (unsigned short int*) L"BOB\r\n");
   while(1);
   return 0;
 }
