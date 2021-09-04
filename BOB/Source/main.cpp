5a947fb475b04888d9232d722f83d15f4ad43823
Levi Hicks <levih@n11.dev>
2021-09-04 18:22:11 -0500

    Getting ready for kernel and etc...

--- BOB/Source/main.cpp
+++ BOB/Source/main.cpp
@@ -318,7 +318,7 @@ typedef struct GOP {
 
 extern "C" unsigned long long boot(void* ImageHandle, SystemTable* SystemTable) {
   SystemTable->ConOut->Reset(SystemTable->ConOut, 1);
-  SystemTable->ConOut->OutputString(SystemTable->ConOut, (unsigned short int*) L"Better Opensource Bootloader (BOB)");
+  SystemTable->ConOut->OutputString(SystemTable->ConOut, (unsigned short int*) L"Better Opensource Bootloader (BOB)\r\n");
   SystemTable->BootServices->Stall(5000000);
   SystemTable->BootServices->SetWatchdogTimer(0, 0, 0, 0);
   return 0;
