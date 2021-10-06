beaafba39af1ff869b1888c04f5bd6aa6b6350d7
Ariston Lorenzo <me@ariston.dev>
2021-10-05 22:15:52 -0400

    LibC: Fix cdefs
    Signed-off-by: Ariston Lorenzo <4tl0renz0@gmail.com>

--- Libraries/LibC/sys/cdefs.h
+++ Libraries/LibC/sys/cdefs.h
@@ -14,11 +14,29 @@
 
 #pragma once
 
+#define _POSIX_VERSION 200809L
+
+#ifndef ALWAYS_INLINE
+#    define ALWAYS_INLINE inline __attribute__((always_inline))
+#endif
+
 #ifdef __cplusplus
-#define __BEGIN_DECLS extern "C" {
-    #define __END_DECLS
-}
+#    ifndef __BEGIN_DECLS
+#        define __BEGIN_DECLS extern "C" {
+#        define __END_DECLS }
+#    endif
 #else
-#define __BEGIN_DECLS
-#define __END_DECLS
+#    ifndef __BEGIN_DECLS
+#        define __BEGIN_DECLS
+#        define __END_DECLS
+#    endif
+#endif
+
+#undef __P
+#define __P(a) a
+
+#define offsetof(type, member) __builtin_offsetof(type, member)
+
+#ifdef __cplusplus
+//extern "C" int main(int, char**);
 #endif
