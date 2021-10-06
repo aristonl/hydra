51873351e754449bdaf678e39d03ccbd05b0ad64
Levi Hicks <levih@n11.dev>
2021-10-05 22:48:13 -0500

    Fix logo spots
    This logo will be replaced soon with a better logo, but as of now we can not use the new one because of tga limitations

--- Inferno/Source/main.cpp
+++ Inferno/Source/main.cpp
@@ -6,12 +6,12 @@ void putpixel(unsigned int x, unsigned int y, unsigned int color) {
 }
 
 void main(Framebuffer* framebuffer, PSFFont* font) {
-  unsigned int* pixel = (unsigned int*)framebuffer->Address;
-  for (unsigned int y=0;y<=framebuffer->Height;y++) {
-    for (unsigned int x=0;x<=framebuffer->Width;x++) {
-      *(unsigned int*)(pixel + x + (y * framebuffer->PPSL)) = 0;
-    }
-  }
+  // unsigned int* pixel = (unsigned int*)framebuffer->Address;
+  // for (unsigned int y=0;y<=framebuffer->Height;y++) {
+  //   for (unsigned int x=0;x<=framebuffer->Width;x++) {
+  //     *(unsigned int*)(pixel + x + (y * framebuffer->PPSL)) = 0;
+  //   }
+  // }
   SetGlobalFramebuffer(framebuffer);
   SetGlobalFont(font);
   printf("Hello, World!\nThis is a hexadecimal: %x", 0xFFaa88);
