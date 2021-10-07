1ad62bfc92e4455f367b9dd429fa051ff3ab0881
Ariston Lorenzo <me@ariston.dev>
2021-10-07 13:01:05 -0400

    Inferno: add IO.h

--- std/Types.h
+++ std/Types.h
@@ -14,6 +14,16 @@ typedef int64_t signed_qword;
 
 typedef unsigned long long size_t;
 
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
 constexpr unsigned KB = 1024;
 constexpr unsigned MB = KB * KB;
 constexpr unsigned GB = KB * KB * KB;
