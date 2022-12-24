8902b79447da9af3ad2c9661e72ac68d5d213953
Ariston Lorenzo <me@ariston.dev>
2022-12-24 18:22:47 -0500

    libc: final changes to build libgcc
    Signed-off-by: Ariston Lorenzo <aristonl@n11.dev>

--- lib/libc/stdio.h
+++ lib/libc/stdio.h
@@ -24,4 +24,14 @@ int sprintf(char* buffer, const char* fmt, ...);
 int putchar(int ch);
 void perror(const char*);
 
+#ifndef SEEK_SET
+#define	SEEK_SET	0	/* set file offset to offset */
+#endif
+#ifndef SEEK_CUR
+#define	SEEK_CUR	1	/* set file offset to current plus offset */
+#endif
+#ifndef SEEK_END
+#define	SEEK_END	2	/* set file offset to EOF plus offset */
+#endif
+
 __END_DECLS
--- lib/libc/string.h
+++ lib/libc/string.h
@@ -8,9 +8,11 @@ __BEGIN_DECLS
 size_t strlen(const char*);
 int strcmp(const char*, const char*);
 int memcmp(const void*, const void*, size_t);
-void memcpy(void*, const void*, size_t);
+void *memcpy(void *, const void *, size_t);
 void* memset(void* str, int c, size_t n);
 
 const char* strerror(int errnum);
+
+char *strcat(char *, const char *);
 
 __END_DECLS
