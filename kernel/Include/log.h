b47053f8e2b1ebcb86c4b4dfca3ad8d8c16f77dd
Ariston Lorenzo <aristonl@n11.dev>
2022-12-25 20:53:30 -0500

    kernel: Some new logging functions
    I'll probably make it like Linux where their printk() function
    where one of the arguments is the log level, like KERN_ERR which
    in turn will log the exact same way as prErr().
    
    Signed-off-by: Ariston Lorenzo <aristonl@n11.dev>

---
+++ kernel/Include/log.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void prInfo(const char* subsystem, const char* message);
+void prErr(const char* subsystem, const char* message);
+void prWarn(const char* subsystem, const char* message);
+void prDebug(const char* subsystem, const char* message);
