aristonl/hydra

commit ca7fdead97b2d70173b3c1c1c23123dd97183404

mm: better malloc implementation
Only issue is, is that the heap is hard coded. Gonna have to figure out
how to determine and init the heap; but this isn't my territory anyways
so ¯\_(ツ)_/¯.

Signed-off-by: Ariston Lorenzo <aristonl@n11.dev>

kernel/Source/Memory/malloc.cpp
@@ -1,21 +1,28 @@
 #include <stdlib.h>
-
+#include <errno.h>
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <log.h>
 
-/*
- *	malloc()
- */ 
-void *malloc(size_t size) {
-	void *p = sbrk(0);
-	void *request = sbrk(size);
-	
-	if (request == (void*) -1) {
-		return NULL;
-	} else {
-		return p;
-	}
+#define heapSize 1000000
+
+static char heap[heapSize];
+static size_t heapPtr = 0;
+
+void* malloc(size_t size)
+{
+  // Check if there is enough space on the heap
+  if (heapPtr + size > heapSize) {
+    // Not enough space on the heap
+    return ENOMEM;
+	prErr("mm", "malloc(): ENOMEM - Not enough space on the heap.");
+  }
+
+  // Allocate memory from the heap
+  void* ptr = &heap[heapPtr];
+  heapPtr += size;
+  return ptr;
 }
 
 struct blockMeta {

commit b4e8109552975d7ba9f19709cff17135b08547a7

mm: comment out malloc()
malloc() is currently making readRTC() pull the complete wrong
dates and times for some odd reason; so I'm commenting this out
until I find a way to fix this or fix the implementation if I
have to.

Fixes: ca7fdea
Signed-off-by: Ariston Lorenzo <aristonl@n11.dev>

kernel/Source/Memory/malloc.cpp
@@ -5,6 +5,8 @@
 
 #include <log.h>
 
+/*
+
 #define heapSize 1000000
 
 static char heap[heapSize];
@@ -36,4 +38,4 @@ struct blockMeta {
 
 void *globalBase = NULL;
 
-
+*/