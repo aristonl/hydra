d762bfb2a6b739f3ef4e0c9d55f25b0965dac12b
Ariston Lorenzo <me@ariston.dev>
2023-03-19 13:18:11 -0400

    acpi: Add rsdp_decriptors for ACPI
    Signed-off-by: Ariston Lorenzo <me@ariston.dev>

---
+++ kernel/Include/drivers/acpi/acpi.h
@@ -0,0 +1,11 @@
+//========= Copyright N11 Software, All rights reserved. ============//
+//
+// File: acpi.h
+// Purpose: Main header file for the ACPI header
+// Maintainer: atl
+//
+//===================================================================//
+
+struct rsdp_descriptor {
+
+}
